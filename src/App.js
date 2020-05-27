e4657733fa5004831c17e96ec91e17eaa2d27341
Glitch (starter-react-template) <none>
2020-05-27 08:28:04 +0000

    🎈👱 Checkpoint ./src/components/Modal.js:15532837/901 ./src/App.js:15532837/17

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <button>Open modal</button>
+            <button onclick="">Open modal</button>
           </p>
           <a
             className="App-link"
---
+++ src/components/Modal.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Modal.css'
+import classNames from 'classnames';
+
+class Modal extends Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
+
+    
+    render() {
+        
+        
+        return <div> 
+            </div>;
+    }
+}
+
+export default Modal;
